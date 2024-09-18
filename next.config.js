4d465ac4b544c26f8990298a73df8f311e76cce1
Kreya <crashaw@gmail.com>
2024-09-18 12:44:29 +0200

    Refactor next.config.js to add async redirects for admin page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/admin",
+        destination: "https://f-gaga.prismic.io/documents/working?l=hr",
+        permanent: false,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
